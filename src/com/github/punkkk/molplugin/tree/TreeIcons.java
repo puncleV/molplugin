ae65644817642a8fd7d8ae6dc34237fe0d7c3c38
Victor Kozlov <punkrus18@gmail.com>
2016-12-15 10:50:06 +0300

    Add icon;

---
+++ src/com/github/punkkk/molplugin/tree/TreeIcons.java
@@ -0,0 +1,9 @@
+package com.github.punkkk.molplugin.tree;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class TreeIcons {
+    public static final Icon FILE = IconLoader.getIcon("/com/github/punkkk/molplugin/tree/icon.png");
+}
